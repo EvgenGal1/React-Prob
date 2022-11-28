068a7aa1a674de51ae9a8425c48dbc20e6a9e621
EvGen Gal <genwolk1@gmail.com>
2022-11-28 03:04:23 +0300

    LSfromUH. Возня с методами масс. для LS ч/з usSt

--- src/Components/probComponents/HooksComponents/LocalStorage/LSfromUH.jsx
+++ src/Components/probComponents/HooksComponents/LocalStorage/LSfromUH.jsx
@@ -34,7 +34,6 @@ function DelEl() {
     </div>
   )
 }
-
 // Изменить эл.
 function ChangeEl() {
   const [arr, setArr] = useState(['Тише', 'мыши', 'кот', 'на', 'крыше']);
@@ -122,7 +121,7 @@ export const LSfromUH = () => {
 
   // Аналогично useState, но первый аргумент является ключом к значению в локальном хранилище.
   const [name, setName] = useLocalStorageUH("name", "Bob");
-  const [arr, setArr] = useLocalStorageUH("arrr", [0, 1, 2]);
+  const [arr, setArr] = useLocalStorageUH("arrr", [0, 1, 2, 3, 4,]);
   const [arrObj, setArrObj] = useLocalStorageUH("arrObj", [
     5,
     { name: 'Василий', age: 35 },
@@ -171,7 +170,8 @@ export const LSfromUH = () => {
   // console.log(
   //   "%carrObj[4] - ", "color: yellow;", arrObj[4],
   // ); // показ fn()
-  console.log('arrObj 0 ', arrObj);
+  // console.log('arrObj 0 ', arrObj);
+  // console.log('arrObj.at(-1) ', arrObj.at(-1)); // ^ получ послед.эл. | 0 - первый эл.
   return (
     <div className="LSfromUH--- accordion" style={{ position: "relative" }}>
       <DelEl />
@@ -207,8 +207,19 @@ export const LSfromUH = () => {
             style={{ background: 'blue', padding: '5px 10px', borderRadius: '5px', cursor: 'pointer' }}
             onClick={() => {
               // ДОБАВЛЕНИЕ
-              // setArr([...arr, arr.length])
-              // setArr([...arr.push("qw")])
+              // setArr([...arr, "+>"]) // ^ добавл эл. в КОНЕЦ
+              setArr(["<+", ...arr,]) // ^ добавл эл. в НАЧАЛО
+              // setArr(["<+", ...arr.slice(0)]) // ^ добавл. эл. в НАЧАЛО
+              // setArr([...arr.slice(0, 0), ...arr.slice(0, 0, "+<", "123")]) // СБРОС в пустой масс.
+              // setArr([...arr.slice(0, 0), "+<", "123", ...arr.slice(1 + 1)]) // ~ добав. в начало, НО один раз
+              // setArr([...arr.slice(0, 9), "+<", "123", ...arr.slice(1 + 1)]) // добавл. весь масс вначало + эл. + 2,3,4
+              // setArr([...arr.slice(0, 9), "+<", "123", ...arr.slice(0 + 0)]) // добавл. масс в начало + эл. + масс. в конце
+              // setArr([arr.splice(0, 0, "<+")])
+              // setArr([...arr.splice(0, 2)]) // вернул 0,1 (до 2 ind), удалил 2,3,4
+              // setArr([...arr.splice(0, 0, "+>")]); // полностью обнул. масс.
+              // setArr([...arr.splice(1, 0, "123")]); // запись в консоль. Вывод и LS - Сброс в []
+              // setArr([...arr.splice(-1, 1)]) // удал ВСЁ кроме ПОСЛЕДН.
+              // setArr([...arr.splice(0, 1)]) // удал ВСЁ кроме ПЕРВОГО.
               // arr.push("+>") // запись в консоль. Вывод и LS - НЕТ
               // setArr(arr.push("+>")) // в консоль 4 // ! ошб. arr.map is not a function 
               // setArr([arr.push("+>")]) // в консоль сначала добав. 4. Вывод и LS - сразу 4, затем 2
@@ -216,14 +227,14 @@ export const LSfromUH = () => {
               // setArr([...arr, arr.push("777")]) // в консоль сначала добав, потом счёт в предполед. Вывод и LS - добавл. счёт в конце, НЕ эл.
               // setArr([arr, arr.push("777")]) // везде созд. подмассивы с эл. с счётом
               // setArr.push("777") // ! ошб. setArr.push is not a function
-              setArr([...arr, "+>"]) // ^ добавл эл. в конце
+
               // УДАЛЕНИЕ
               // setArr([...arr, arr.shift()]) // добав. 0 в конце  
               // const removeItem = (index) => {
-              // setArr([
-              //   ...arr.slice(0, 1),
-              //   ...arr.slice(1 + 1)
-              // ]);
+              // setArr([...arr.slice(0, 1)]); // копия с 0 до 1 по ind, остал. удал.
+              // setArr([ ...arr.slice(1 + 2)]); // удал 0,1,2
+              // setArr([...arr.slice(0, 0), ...arr.slice(1 + 2)]); // удал 0,1,2
+              // setArr([...arr.slice(0, 1), ...arr.slice(1 + 2)]); // удал 1,2
               // }
               console.log('arr onClick ', arr);
             }}
@@ -241,13 +252,21 @@ export const LSfromUH = () => {
               // arr.shift();
               // setArr([...arr, -arr.length])
             }}
-          >+доп+</button>
-          <div style={{
-            display: "flex",
-            justifyContent: "center"
-          }}>
+          >
+            +доп+
+          </button>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center"
+            }}>
             {arr.map((ar) => (
-              <span key={Math.random().toString().substring(2, 7)} style={{ marginLeft: "5px" }}>{ar}</span>
+              <span
+                key={Math.random().toString().substring(2, 7)}
+                style={{ marginLeft: "5px" }}
+              >
+                {ar}
+              </span>
             ))}
           </div>
         </div>
